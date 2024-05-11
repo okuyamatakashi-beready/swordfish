d65501ac8b11361abf6c71cc182423862bda868e
奥山貴氏 <>
2024-05-11 18:20:51 +0900

    mac mini toppage

--- index.php
+++ index.php
@@ -79,160 +79,245 @@
 		<span></span>
 	</div>
 
-	<div id="mv">
-		<div class="mv__wrapper">
-			<video id="videoTop" autoplay muted playsinline loop width="1920px" height="1080px">
-				<source src="<?php echo get_template_directory_uri(); ?>/images/video/pc_mv.mp4" type="video/mp4">
-			</video>
-			<p class="catch">
-				海で見つける<br>
-				まだ見ぬ新しい自分
-			</p>
+	<main>
+		<div id="mv">
+			<div class="mv__wrapper">
+				<video id="videoTop" autoplay muted playsinline loop width="1920px" height="1080px">
+					<source src="<?php echo get_template_directory_uri(); ?>/images/video/pc_mv.mp4" type="video/mp4">
+				</video>
+				<p class="catch">
+					海で見つける<br>
+					まだ見ぬ新しい自分
+				</p>
+			</div>
 		</div>
-	</div>
-	<!-- //mv -->
+		<!-- //mv -->
 
-	<section id="about">
-		<div class="left_container">
-			<div class="flex">
-				<div class="about__slider">
-					<div class="about__slider--slide">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide01.jpg">
+		<section id="about">
+			<div class="left_container">
+				<div class="flex">
+					<div class="about__slider">
+						<div class="about__slider--slide">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide01.jpg">
+						</div>
+						<div class="about__slider--slide">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide02.jpg">
+						</div>
+						<div class="about__slider--slide">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide03.jpg">
+						</div>
 					</div>
-					<div class="about__slider--slide">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide02.jpg">
-					</div>
-					<div class="about__slider--slide">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/top/about_slide03.jpg">
+
+					<div class="about__right">
+						<div class="section__content">
+							<h2 class="section__content--ttl">
+								<small>Shop Infomation</small>
+								<span>Sword Fishとは</span>
+							</h2>
+
+							<p class="section__content--text">
+								サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
+								サイトの紹介文が入りますサイトの紹介文が入ります
+								サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
+								サイトの紹介文が入りますサイトの紹介文が入ります
+								サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
+							</p>
+
+							<a href="/" class="common__btn">詳しく見る</a>
+						</div>
 					</div>
 				</div>
+				<!-- //flex -->
+			</div>
+		</section>
 
-				<div class="about__right">
+		<div class="scroll__textWrapper">
+			<div class="big__slideText">SWORD FISH DIVING SHOP</div>
+			<div class="big__slideText">SWORD FISH DIVING SHOP</div>
+		</div>
+
+		<section id="menu">
+			<div class="menu__wrapper flex">
+				<div class="menu__wrapper--left">
+					<ul class="flex">
+						<li class="aonodokutsu">
+							<a href="/" class="common__btn common__btn--white">青の洞窟・恩納村コース</a>
+						</li>
+						<li class="sup">
+							<a href="/" class="common__btn common__btn--white">SUP・シーカヤック</a>
+						</li>
+						<li class="marinactivity">
+							<a href="/" class="common__btn common__btn--white">マリンアクティビティ</a>
+						</li>
+						<li class="pala">
+							<a href="/" class="common__btn common__btn--white">パラセーリング</a>
+						</li>
+						<li class="bbq">
+							<a href="/" class="common__btn common__btn--white">セットメニュー</a>
+						</li>
+						<li class="fishing">
+							<a href="/" class="common__btn common__btn--white">フィッシング＆グリル</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="menu__wrapper--right">
 					<div class="section__content">
 						<h2 class="section__content--ttl">
-							<small>Shop Infomation</small>
-							<span>Sword Fishとは</span>
+							<small>Menu</small>
+							<span>メニュー</span>
 						</h2>
 
 						<p class="section__content--text">
-							サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
-							サイトの紹介文が入りますサイトの紹介文が入ります
-							サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
-							サイトの紹介文が入りますサイトの紹介文が入ります
-							サイトの紹介文が入りますサイトの紹介文が入りますサイトの紹介文が入ります
+							ソードフィッシュでは、青の洞窟ツアーやシーカヤックツアー、マリンスポーツなどを通して、
+							沖縄のステキな思い出をたくさん作っていただきたい！また家族や友人との大切な思い出にしてほしい！
+							そんな気持ちでお客様をお待ちしております！お客様の笑顔に出会えることが私たちの一番の楽しみです！
 						</p>
 
 						<a href="/" class="common__btn">詳しく見る</a>
 					</div>
 				</div>
 			</div>
-			<!-- //flex -->
+		</section>
+
+		<div class="scroll__textWrapper">
+			<div class="big__slideText">SWORD FISH DIVING SHOP</div>
+			<div class="big__slideText">SWORD FISH DIVING SHOP</div>
 		</div>
-	</section>
 
+		<div id="feature">
+			<div class="container">
+				<div class="feature__content feature__content--01 feature__content--left flex">
+					<div class="feature__text feature__text--left flex">
+						<span>特徴01</span>
+						<div class="feature__ttl">
+							<strong>当日予約OK！</strong>
+							<p>
+								当日のご予約でも大丈夫！お電話にて、<br>
+								ご予約の上、各コースの集合時間をご確認頂き、<br>
+								集合時間に間に合うようお越しください。
+							</p>
+						</div>
+					</div>
 
-	<span class="big__slideText">SWORD FISH DIVING SHOP</span>
+					<div class="feature__images feature__images--01"></div>
+				</div>
 
-	<section id="menu">
-		<div class="menu__wrapper">
-			<div class="menu__wrapper--right">
+				<div class="feature__content feature__content--02 feature__content--right flex">
+					<div class="feature__text feature__text--right flex">
+						<span>特徴02</span>
+						<div class="feature__ttl">
+							<strong>水着だけでOK！</strong>
+							<p>
+								持ち物は、水着とタオルだけでOK！<br>
+								シュノーケルの水中メガネ、ウエットスーツなどの<br>
+								必要器材は各コース料金に含まれております。
+							</p>
+						</div>
+					</div>
+
+					<div class="feature__images feature__images--02"></div>
+				</div>
+
+				<div class="feature__content feature__content--03 feature__content--left flex">
+					<div class="feature__text feature__text--left flex">
+						<span>特徴03</span>
+						<div class="feature__ttl">
+							<strong>雨の日でも開催！</strong>
+							<p>
+								雨が降っていても海の状態がよければツアーは開催いたします。<br>
+								ただ台風や海洋状況によっては中止になる場合がございます。
+							</p>
+						</div>
+					</div>
+
+					<div class="feature__images feature__images--03"></div>
+				</div>
+			</div>
+		</div>
+
+		<section id="staff">
+			<div class="container">
+				<h2 class="sec_ttl--center">
+					<span>Staff</span>
+					<strong>スタッフ</strong>
+				</h2>
+
 				<ul class="flex">
+				<?php
+					$paged = get_query_var('paged') ? get_query_var('paged') : 1 ;
+					$args = array(
+						'posts_per_page' => '1',
+						'post_status' => 'publish',
+						'paged' => $paged,
+						'post_type' => 'swordfish-staff',
+					);
+					?>
+					<?php $my_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
+
+						<?php if ( $my_query->have_posts() ) :
+
+							while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+
+					<?php 
+						$name = get_field('name');
+						$job = get_field('job');
+						$img = get_field('img');
+					?>
 					<li>
-						<a href="/" class="common__btn common__btn--white">青の洞窟・恩納村コース</a>
+						<div class="member__thumb" style="background-image:url(<?php echo $img;?>);"></div>
+						<div class="member__text">
+							<b><?php echo $name;?></b>
+							<span><?php echo $job;?></span>
+						</div>
 					</li>
-					<li>
-						<a href="/" class="common__btn common__btn--white">SUP・シーカヤック</a>
-					</li>
-					<li>
-						<a href="/" class="common__btn common__btn--white">マリンアクティビティ</a>
-					</li>
-					<li>
-						<a href="/" class="common__btn common__btn--white">パラセーリング</a>
-					</li>
-					<li>
-						<a href="/" class="common__btn common__btn--white">セットメニュー</a>
-					</li>
-					<li>
-						<a href="/" class="common__btn common__btn--white">フィッシング＆グリル</a>
-					</li>
+					<?php endwhile; ?>
+					<?php endif; ?>
+					<?php wp_reset_postdata(); ?>	
 				</ul>
 			</div>
+		</section>
 
-			<div class="menu__wrapper--left">
-				<div class="section__content">
-					<h2 class="section__content--ttl">
-						<small>Menu</small>
-						<span>メニュー</span>
-					</h2>
+		<section id="news">
+			<div class="container">
+				<h2 class="sec_ttl--center">
+					<span>News</span>
+					<strong>お知らせ</strong>
+				</h2>
 
-					<p class="section__content--text">
-						ソードフィッシュでは、青の洞窟ツアーやシーカヤックツアー、マリンスポーツなどを通して、
-						沖縄のステキな思い出をたくさん作っていただきたい！また家族や友人との大切な思い出にしてほしい！
-						そんな気持ちでお客様をお待ちしております！お客様の笑顔に出会えることが私たちの一番の楽しみです！
-					</p>
+				<ul class="flex">
+				<?php
+					$paged = get_query_var('paged') ? get_query_var('paged') : 1 ;
+					$args = array(
+						'posts_per_page' => '6',
+						'post_status' => 'publish',
+						'paged' => $paged,
+						'post_type' => 'post',
+					);
+					?>
+					<?php $my_query = new WP_Query( $args ); ?><!-- クエリの指定 -->
 
-					<a href="/" class="common__btn">詳しく見る</a>
-				</div>
+						<?php if ( $my_query->have_posts() ) :
+
+							while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+
+						<li style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>)">
+							<a href="<?php the_permalink(); ?>">
+								<div class="post_ttl">
+									<span class="cat">グルメ</span>
+									<h3><?php the_title();?></h3>
+								</div>
+							</a>
+						</li>
+					<?php endwhile; ?>
+					<?php endif; ?>
+					<?php wp_reset_postdata(); ?>	
+
+				</ul>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<span class="big__slideText">SWORD FISH DIVING SHOP</span>
-
-	<div id="feature">
-		<div class="container">
-			<div class="feature__content feature__content--left flex">
-				<div class="feature__text feature__text--left">
-					<span>特徴01</span>
-					<div class="feature__ttl">
-						<strong>当日予約OK！</strong>
-						<p>
-							当日のご予約でも大丈夫！お電話にて、<br>
-							ご予約の上、各コースの集合時間をご確認頂き、<br>
-							集合時間に間に合うようお越しください。
-						</p>
-					</div>
-				</div>
-
-				<div class="feature__images feature__images--01"></div>
-			</div>
-
-			<div class="feature__content feature__content--right flex">
-				<div class="feature__text feature__text--left">
-					<span>特徴02</span>
-					<div class="feature__ttl">
-						<strong>水着だけでOK！</strong>
-						<p>
-							持ち物は、水着とタオルだけでOK！<br>
-							シュノーケルの水中メガネ、ウエットスーツなどの<br>
-							必要器材は各コース料金に含まれております。
-						</p>
-					</div>
-				</div>
-
-				<div class="feature__images feature__images--02"></div>
-			</div>
-
-			<div class="feature__content feature__content--left flex">
-				<div class="feature__text feature__text--left">
-					<span>特徴03</span>
-					<div class="feature__ttl">
-						<strong>雨の日でも開催！</strong>
-						<p>
-							雨が降っていても海の状態がよければツアーは開催いたします。<br>
-							ただ台風や海洋状況によっては中止になる場合がございます。
-						</p>
-					</div>
-				</div>
-
-				<div class="feature__images feature__images--03"></div>
-			</div>
-		</div>
-	</div>
-
-<main>
-	
-</main>
+	</main>
 	
 
 </body>
@@ -241,11 +326,13 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.arctext.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/infiniteslidev2.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.2/jquery.matchHeight-min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/index.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+
 <script src="<?php echo get_template_directory_uri(); ?>/js/movie.js"></script>
 	
 </html>
--- js/index.js
+++ js/index.js
@@ -4,16 +4,39 @@
 
 $(document).ready(function(){
     $('.about__slider').slick({
-        slidesToShow: 2.4, // 一度に表示するスライド数
-        slidesToScroll: 1, // 一度にスクロールするスライド数
-        autoplay: true, // 自動再生を有効にする
-        autoplaySpeed: 0, // 自動再生のスピード（0に設定して無限スクロールのように見せる）
-        speed: 5000, // アニメーションの速度（ms）
-        cssEase: 'linear', // アニメーションの種類（リニアに設定して等速でスクロール）
-        infinite: true, // 無限スクロール
-        pauseOnHover: false, // ホバー時に停止しない
-        pauseOnFocus: false, // フォーカス時に停止しない
-        arrows: false, // 矢印を非表示
-        dots: false // ドットナビゲーションを非表示
+        slidesToShow: 2.4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase: 'linear',
+        infinite: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        arrows: false,
+        dots: false
+    });
+});
+
+/*
+	SWORD FISH SHOPの無限文字
+*/
+$(document).ready(function() {
+    // 背景文字の無限ループ
+    $('.scroll__textWrapper').infiniteslide({
+        'speed': 100,
+        'direction': 'left',
+        'pauseonhover': false,
+        'responsive': true,
+        'clone': 100
+    });
+});
+
+/*
+	matchHeight
+*/
+$(function() {
+	$('.feature__text').matchHeight({
+        target: $('.feature__images')
     });
 });
